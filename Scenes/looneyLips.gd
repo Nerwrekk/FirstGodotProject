a443595a3d8a3fa9cf9e423c32606aaa7196f6b2
Alexander Tanaskovic <alexander.tanaskovic@hotmail.se>
2020-08-06 10:35:33 +0200

    Added Player Input.

--- Scenes/looneyLips.gd
+++ Scenes/looneyLips.gd
@@ -1,11 +1,22 @@
 extends Control
 
-onready var label = $Label
 export var text = ""
+
 var words = ["Fork", "Spoon", "Spork"]
 var story = "Once every full moon a %s finds a %s and mates. 6 months later a %s is born"
 
+onready var display_text = $VBoxContainer/DisplayText
+onready var player_input = $VBoxContainer/PlayerInput
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label.text = story % words
+	display_text.text = story % words
 
+func update_DisplayText(new_text):
+	display_text.text = player_input.text;
+	player_input.clear()
+
+func _on_PlayerInput_text_entered(new_text):
+	update_DisplayText(new_text)
+	
